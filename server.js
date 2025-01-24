7d82e1d855686ce3b5378ba5302949bcbf7a4eea
justin <justin20745413@gmail.com>
2025-01-24 11:34:53 +0800

    fix:調整api

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.use(cors({
     origin: [
         'http://localhost:5173',
         'http://localhost:5174',
-        'https://justin20745413.github.io/'
+        'https://justin20745413.github.io/',
+        'https://justin20745413.github.io/test03/'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
